9e2b6f6212b9c71b5d2b0937aeb89d843ceac595
iAmNsengi <iamnsengi@icloud.com>
2025-05-10 14:29:25 +0200

    feat: add expo logo and splash screen

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -23,23 +23,40 @@ const TabIcon = ({
   return focused ? (
     <ImageBackground
       source={focused ? images.highlight : ""}
-      className="flex flex-row w-full flex-1 min-w-[90px] min-h-14 mt-6 justify-center items-center rounded-full overflow-hidden px4"
+      className="flex flex-row w-full flex-1 min-w-[90px] min-h-14 mt-4 justify-center items-center rounded-full overflow-hidden"
     >
       <Image source={icon} tintColor={"#151312"} className="size-5" />
-      <Text className="text-secondary text-base font-semibold ml-2">
-        {title}
-      </Text>
+      <Text className="text-secondary text-xs font-semibold ml-2">{title}</Text>
     </ImageBackground>
   ) : (
     <View className="size-full flex flex-col mt-6 text-xs justify-center items-center rounded full">
-      <Image source={icon} tintColor={"#151312"} className="size-5" />
+      <Image source={icon} tintColor={"#fff"} className="size-5" />
     </View>
   );
 };
 
 const _Layout = () => {
   return (
-    <Tabs>
+    <Tabs
+      screenOptions={{
+        tabBarShowLabel: false,
+        tabBarItemStyle: {
+          width: "100%",
+          height: "100%",
+          justifyContent: "center",
+          alignItems: "center",
+        },
+        tabBarStyle: {
+          backgroundColor: "#0f0D23",
+          // borderRadius: 50,
+          // marginHorizontal: 20,
+          // marginBottom: 36,
+          // height: 52,
+          // position: "absolute",
+          // borderWidth: 1,
+        },
+      }}
+    >
       <Tabs.Screen
         name="index"
         options={{
