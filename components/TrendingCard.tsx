be3723e89b985c9afa5ea732ccb9e2ccc5f43851
iAmNsengi <iamnsengi@icloud.com>
2025-05-20 16:44:27 +0200

    add: trending card masked view ui

--- components/TrendingCard.tsx
+++ components/TrendingCard.tsx
@@ -1,3 +1,5 @@
+import { images } from "@/constants/images";
+import MaskedView from "@react-native-masked-view/masked-view";
 import { Link } from "expo-router";
 import React from "react";
 import { Image, StyleSheet, Text, TouchableOpacity, View } from "react-native";
@@ -5,16 +7,31 @@ import { Image, StyleSheet, Text, TouchableOpacity, View } from "react-native";
 const TrendingCard = ({ movies }: { movies: TrendingMovie[] }) => {
   return (
     <View>
-      {movies.map((movie) => (
+      {movies.map((movie, i) => (
         <Link
           href={`/movies/${movie.movie_id}`}
           asChild
-          className="flex flex-col"
+          className="flex flex-row"
         >
-          <TouchableOpacity className="w-40">
-            <Image source={{ uri: movie.poster_url }} className="w-12 h-20" />
-            <Text className="text-white">{movie.title} </Text>
-          </TouchableOpacity>
+          <View>
+            <TouchableOpacity className="w-40">
+              <Image
+                source={{ uri: movie.poster_url }}
+                className="w-32 h-40 rounded-lg"
+                resizeMode="cover"
+              />
+              <Text className="text-white text-sm">{movie.title} </Text>
+            </TouchableOpacity>
+            <View className="absolute bottom-9 left-0">
+              <MaskedView
+                maskElement={
+                  <Text className="font-bold text-6xl text-white">{i + 1}</Text>
+                }
+              >
+                <Image source={images.rankingGradient} className="size-14" />
+              </MaskedView>
+            </View>
+          </View>
         </Link>
       ))}
     </View>
